da1c74d2f012365ae26c4c57bffd969cde921f1b
srgsuh <srg.suhoverhov@gmail.com>
2025-09-18 10:36:23 +0300

    Added tests for the case of an array with possible duplicates

--- test/test_main.py
+++ test/test_main.py
@@ -1,22 +1,22 @@
 import pytest
 from main import bSearchSortedList
 
-nums: list[int] = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+nums: list[int] = [10, 10, 10, 20, 30, 30, 40, 50, 60, 70, 80, 90, 90, 90, 90]
 
 def test_bSearchSortedList_found_10():
     assert bSearchSortedList(nums, 10) == 0
 
 def test_bSearchSortedList_found_30():
-    assert bSearchSortedList(nums, 30) == 2
+    assert bSearchSortedList(nums, 30) == 4
     
 def test_bSearchSortedList_found_90():
-    assert bSearchSortedList(nums, 90) == 8
+    assert bSearchSortedList(nums, 90) == 11
 
 def test_bSearchSortedList_not_found_1():
     assert bSearchSortedList(nums, 1) == -1
     
 def test_bSearchSortedList_not_found_43():
-    assert bSearchSortedList(nums, 43) == -5
+    assert bSearchSortedList(nums, 43) == -8
 
 def test_bSearchSortedList_not_found_99():
-    assert bSearchSortedList(nums, 99) == -10
+    assert bSearchSortedList(nums, 99) == -16
