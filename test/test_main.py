e284c943719d82cf4fb0b179dff05f21e0366118
srgsuh <srg.suhoverhov@gmail.com>
2025-09-21 10:07:55 +0300

    Add several edge test cases

--- test/test_main.py
+++ test/test_main.py
@@ -23,3 +23,18 @@ def test_bSearchSortedList_not_found_43():
 
 def test_bSearchSortedList_not_found_99():
     assert bSearchSortedList(nums, 99) == -16
+    
+def test_bSearchSortedList_ten_zeroes_0():
+    assert bSearchSortedList([0,0,0,0,0,0,0,0,0,0], 0) == 0
+    
+def test_bSearchSortedList_ten_zeroes_one():
+    assert bSearchSortedList([0,0,0,0,0,0,0,0,0,0], 1) == -11
+
+def test_bSearchSortedList_ten_zeroes_minus_one():
+    assert bSearchSortedList([0,0,0,0,0,0,0,0,0,0], -1) == -1
+    
+def test_bSearchSortedList_012_one():
+    assert bSearchSortedList([0,1,2], 1) == 1
+
+def test_bSearchSortedList_012_two():
+    assert bSearchSortedList([0,1,2], 2) == 2
