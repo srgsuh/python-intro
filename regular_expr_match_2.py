22f2704523433d1965b58112e900a72176181f2a
srgsuh <srg.suhoverhov@gmail.com>
2025-09-23 20:46:50 +0300

    Regular expr matching solution with lru_cache

--- regular_expr_match_2.py
+++ regular_expr_match_2.py
@@ -1,41 +1,27 @@
 #https://leetcode.com/problems/regular-expression-matching/description/
+from functools import lru_cache
 
 class Solution(object):
-    def cmp(self, r, c):
-        if self.dp[r][c] == -1:
-            isJoker = c < self.cols - 1 and self.pattern[c + 1] == '*'
-            if c == self.cols:
-                self.dp[r][c] = 1 if r == self.rows else 0
-            elif r == self.rows:
-                self.dp[r][c] = 1 if isJoker and self.cmp(r, c + 2) else 0
-            else:
-                doMatch = self.pattern[c] == '.' or self.pattern[c] == self.str[r]
+    def isMatch(self, str: str, pattern: str) -> bool:
+        rows: int = len(str)
+        cols: int = len(pattern)
+        joker: str = '*'
+        
+        def symbolMatch(s: str, p: str) -> bool:
+            return p == '.' or p == s
                 
-                if isJoker:
-                    if (
-                        self.cmp(r, c + 2) or
-                        doMatch and (
-                            self.cmp(r + 1, c + 2) or self.cmp(r + 1, c)
-                        ) 
-                    ):
-                        self.dp[r][c] = 1
-                    else:
-                        self.dp[r][c] = 0
-                else:
-                    self.dp[r][c] = doMatch and self.cmp(r + 1, c + 1)
+        @lru_cache
+        def dp(r: int, c: int) -> bool:
+            if (c == cols):
+                return r == rows
+            if (r == rows):
+                return c + 1 < cols and pattern[c + 1] == joker and dp(r, c + 2)
+            if c + 1 < cols and pattern[c + 1] == joker:
+                return dp(r, c + 2) or (symbolMatch(str[r], pattern[c]) and (
+                    dp(r + 1, c) or dp(r + 1, c + 1)
+                ))
+            else:
+                return symbolMatch(str[r], pattern[c]) and dp(r + 1, c + 1)
+            return False
             
-        return 1 == self.dp[r][c]
-    
-    def isMatch(self, s, p):
-        """
-        :type s: str
-        :type p: str
-        :rtype: bool
-        """
-        self.str = s
-        self.pattern = p
-        self.rows = len(s)
-        self.cols = len(p)
-        self.dp = [[-1 for j in range(1 + self.cols)] for i in range(1 + self.rows)]
-        self.dp[self.rows][self.cols] = 1
-        return self.cmp(0, 0)
+        return dp(0, 0)
