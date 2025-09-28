31da20dd943c941f9dcb920c15d08aed82a62741
srgsuh <srg.suhoverhov@gmail.com>
2025-09-28 19:29:28 +0300

    get_numbers_range implementation using bisect package methods

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-numbers: list[int] = [30, 20, 50, 3]
+import bisect
 
-def getNumbersRange(arr: list[int], min: int, max: int) -> list[int]:
-    res: list[int] = []
-    for v in arr:
-        if (min <= v <= max):
-            res.append(v)
-    return res
-            
-    
+numbers: list[int] = []
+bisect.insort(numbers, 30)
+bisect.insort(numbers, 20)
+bisect.insort(numbers, 50)
+bisect.insort(numbers, 3)
+bisect.insort(numbers, 10)
+
+def get_numbers_range(arr: list[int], min_val: int, max_val: int) -> list[int]:
+    """Returns a list of numbers from the given array that are within the given range inclusively."""
+    left: int = bisect.bisect_left(arr, min_val)
+    right: int = bisect.bisect_right(arr, max_val)
+    return arr[left:right]
 
 if __name__ == '__main__':
     print(numbers)
-    print(getNumbersRange(numbers, 25, 35))
+    print(get_numbers_range(numbers, 0, 1))
