4ec7c502b89610f54ed133cbd85c09993ce7181d
srgsuh <srg.suhoverhov@gmail.com>
2025-09-28 19:58:04 +0300

    Implementation using SortedList

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-import bisect
+import sortedcontainers as sc
+from sortedcontainers import SortedList
 
-numbers: list[int] = []
-bisect.insort(numbers, 30)
-bisect.insort(numbers, 20)
-bisect.insort(numbers, 50)
-bisect.insort(numbers, 3)
-bisect.insort(numbers, 10)
+numbers: sc.SortedList = sc.SortedList()
+numbers.add(30)
+numbers.add(20)
+numbers.add(50)
+numbers.add(3)
+numbers.add(10)
 
-def get_numbers_range(arr: list[int], min_val: int, max_val: int) -> list[int]:
+def get_numbers_range(arr: SortedList, min_val: int, max_val: int) -> list[int]:
     """Returns a list of numbers from the given array that are within the given range inclusively."""
-    left: int = bisect.bisect_left(arr, min_val)
-    right: int = bisect.bisect_right(arr, max_val)
+    left: int = arr.bisect_left(min_val)
+    right: int = arr.bisect_right(max_val)
     return arr[left:right]
 
 if __name__ == '__main__':
     print(numbers)
-    print(get_numbers_range(numbers, 0, 1))
+    print(get_numbers_range(numbers, 20, 30))
