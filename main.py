541ce53e285bf929a673f5b0e47c2b2bf6a4c90a
srgsuh <srg.suhoverhov@gmail.com>
2025-09-28 20:18:28 +0300

    Introduction of SortedKeyList class

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 import sortedcontainers as sc
-from sortedcontainers import SortedList
 
-numbers: sc.SortedList = sc.SortedList()
-numbers.add(30)
-numbers.add(20)
-numbers.add(50)
-numbers.add(3)
-numbers.add(10)
-
-def get_numbers_range(arr: SortedList, min_val: int, max_val: int) -> list[int]:
-    """Returns a list of numbers from the given array that are within the given range inclusively."""
-    left: int = arr.bisect_left(min_val)
-    right: int = arr.bisect_right(max_val)
-    return arr[left:right]
+strs: sc.SortedKeyList = sc.SortedKeyList(key=str.casefold)
+strs.add("AbCd")
+strs.add("aBc")
+strs.add("abcd")
+strs.add("AC")
 
 if __name__ == '__main__':
-    print(numbers)
-    print(get_numbers_range(numbers, 20, 30))
+    print(strs)
