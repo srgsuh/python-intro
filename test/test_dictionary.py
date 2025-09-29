861c5e716b3a08974b1ac80178244ee5fc7957d6
srgsuh <srg.suhoverhov@gmail.com>
2025-09-29 18:29:29 +0300

    Add a test for the get_words_by_prefix method of Dictionary class

--- test/test_dictionary.py
+++ test/test_dictionary.py
@@ -25,3 +25,23 @@ class TestDictionary(ut.TestCase):
     def test_add_word_ok(self):
         self.words.add_word(self.outer_word)
         self.assertIn(self.outer_word, self.words.get_all_words())
+
+    def test_get_words_by_prefix_01(self):
+        car_prefixed = sorted(
+            [self.word_list[k] for k in [1, 2, 3, 4, 5]], key=lambda w: w.casefold()
+        )
+        self.assertEqual(self.words.get_words_by_prefix("car"), car_prefixed)
+
+    def test_get_words_by_prefix_02(self):
+        cart_prefixed = sorted([self.word_list[k] for k in [2, 3, 4]], key=lambda w: w.casefold())
+        self.assertEqual(self.words.get_words_by_prefix("cart"), cart_prefixed)
+
+    def test_get_words_by_prefix_03(self):
+        self.assertEqual(self.words.get_words_by_prefix(self.outer_word), [])
+
+    def test_get_words_by_prefix_04(self):
+        self.assertEqual(self.words.get_words_by_prefix(""), self.test_get_all_words())
+
+if __name__ == '__main__':
+    ut.main()
+
