f25591d9a9abf4cf6d942b633e7d45e98e3d4518
srgsuh <srg.suhoverhov@gmail.com>
2025-09-30 16:29:05 +0300

    Add implementation of Club class

--- main.py
+++ main.py
@@ -33,14 +33,19 @@ class Club:
         Return a list of the club members, whose age is within the inclusive range: [min_age, max_age].
         Sort the result by age and id.
         """
-        left: int = self.__sorted_key_list.bisect_left(Person(-1, min_age))
-        right: int = self.__sorted_key_list.bisect_left(Person(-1, max_age + 1))
-        return list(self.__sorted_key_list[left:right])
+        persons: list[Person] = []
+        if len(self.__sorted_set) > 0:
+            min_id: int = self.__sorted_set[0].id
+            left: int = self.__sorted_key_list.bisect_left(Person(min_id, min_age))
+            right: int = self.__sorted_key_list.bisect_left(Person(min_id, max_age + 1))
+            persons = list(self.__sorted_key_list[left:right])
+
+        return persons
 
 if __name__ == '__main__':
-    person1: Person = Person(100, 29)
+    person1: Person = Person(100, 55)
     person2: Person = Person(500, 25)
-    person3: Person = Person(300, 30)
+    person3: Person = Person(300, 29)
     person3_1 = Person(300, 55)
 
     print(f"person1 < person2 == {person1 < person2}")
@@ -49,9 +54,9 @@ if __name__ == '__main__':
     club: Club = Club()
     club.add_person(person1)
     club.add_person(person2)
-    club.add_person(person3_1)
+    club.add_person(person3)
 
     print(f"All members sorted by id: {club.get_all_sorted_by_id()}")
     print(f"All members sorted by age, id: {club.get_all_sorted_by_age_id()}")
-    print(f"Persons between 25 and 29: {club.get_persons_by_age(25, 29)}")
+    print(f"Persons between 20 and 29: {club.get_persons_by_age(20, 29)}")
     print(f"Persons between 77 and 79: {club.get_persons_by_age(77, 79)}")
