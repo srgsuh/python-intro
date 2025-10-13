5b28f371570fbe9c35316c5c787c38fe8318b50e
srgsuh <srg.suhoverhov@gmail.com>
2025-10-13 17:13:41 +0300

    Add tests for update and pop methods of the MyDict class

--- test_my_dict.py
+++ test_my_dict.py
@@ -10,6 +10,8 @@ class TestMyDict(ut.TestCase):
         self.emptyDict: MyDict[str, int] = MyDict()
     def test_get_item(self):
         self.assertEqual(2, self.myDict['b'])
+        self.assertRaises(KeyError, lambda: self.myDict['c'])
+        self.assertRaises(KeyError, lambda: self.emptyDict['a'])
     def test_set_item(self):
         self.myDict['c'] = 3
         self.assertEqual(3, self.myDict['c'])
@@ -22,6 +24,8 @@ class TestMyDict(ut.TestCase):
         self.assertEqual(1, self.myDict.get('a'))
         self.assertEqual(None, self.myDict.get('c'))
         self.assertEqual(10, self.myDict.get('c', 10))
+        self.assertEqual(None, self.emptyDict.get('a'))
+        self.assertEqual(1, self.emptyDict.get('a', 1))
     def test_set_default(self):
         self.assertEqual(1, self.myDict.setdefault('a', 10))
         self.assertEqual(1, self.myDict['a'])
@@ -39,6 +43,19 @@ class TestMyDict(ut.TestCase):
         my_values: list[int] = self.myDict.values()
         self.assertTrue(1 in my_values and 2 in my_values and len(my_values) == 2)
         self.assertEqual(0, len(self.emptyDict.values()))
+    def test_update(self):
+        self.myDict.update('a', 8)
+        self.assertEqual(8, self.myDict['a'])
+        self.myDict.update('c', 11)
+        self.assertEqual(11, self.myDict['c'])
+        self.emptyDict.update('a', 1)
+        self.assertEqual(1, self.emptyDict['a'])
+    def test_pop(self):
+        self.assertEqual(1, self.myDict.pop('a', 10))
+        self.assertEqual(2, self.myDict.pop('b'))
+        self.assertRaises(KeyError, lambda: self.myDict['a'])
+        self.assertEqual(2, self.myDict.pop('a', 2))
+        self.assertRaises(KeyError, lambda: self.myDict.pop('a'))
 
 
 
