5e574c08d3617a24a920517268b5e9b5253f3474
srgsuh <srg.suhoverhov@gmail.com>
2025-10-13 16:38:58 +0300

    Add tests for keys, items and values methods of MyDict class

--- test_my_dict.py
+++ test_my_dict.py
@@ -1,11 +1,13 @@
 import unittest as ut
 from main import MyDict
+from collections import Counter
 
 class TestMyDict(ut.TestCase):
     def setUp(self):
         self.myDict: MyDict[str, int] = MyDict()
         self.myDict['a'] = 1
         self.myDict['b'] = 2
+        self.emptyDict: MyDict[str, int] = MyDict()
     def test_get_item(self):
         self.assertEqual(2, self.myDict['b'])
     def test_set_item(self):
@@ -15,6 +17,7 @@ class TestMyDict(ut.TestCase):
         self.assertEqual(10, self.myDict['a'])
     def test_len(self):
         self.assertEqual(2, len(self.myDict))
+        self.assertEqual(0, len(self.emptyDict))
     def test_get(self):
         self.assertEqual(1, self.myDict.get('a'))
         self.assertEqual(None, self.myDict.get('c'))
@@ -24,6 +27,18 @@ class TestMyDict(ut.TestCase):
         self.assertEqual(1, self.myDict['a'])
         self.assertEqual(10, self.myDict.setdefault('c', 10))
         self.assertEqual(10, self.myDict['c'])
+    def test_keys(self):
+        my_keys: list[str] = self.myDict.keys()
+        self.assertTrue('a' in my_keys and 'b' in my_keys and len(my_keys) == 2)
+        self.assertEqual(0, len(self.emptyDict.keys()))
+    def test_items(self):
+        my_items: list[tuple[str, int]] = self.myDict.items()
+        self.assertTrue(('a', 1) in my_items and ('b', 2) in my_items and len(my_items) == 2)
+        self.assertEqual(0, len(self.emptyDict.items()))
+    def test_values(self):
+        my_values: list[int] = self.myDict.values()
+        self.assertTrue(1 in my_values and 2 in my_values and len(my_values) == 2)
+        self.assertEqual(0, len(self.emptyDict.values()))
 
 
 
