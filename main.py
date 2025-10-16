9a7b2e658a248c2c0777538a70ecf756a634c5f3
srgsuh <srg.suhoverhov@gmail.com>
2025-10-16 10:05:42 +0300

    Basic implementation of MySTackInt methods

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
+import math
 
 class MyStackInt:
     def __init__(self):
-        pass
+        self._data = []
+        self._max_values = [-math.inf]
     
-    def push(val: int) -> None:
+    def push(self, val: int) -> None:
         """Add new element to the top of the stack"""
-        raise NotImplementedError()
+        self._data.append(val)
+        if val >= self._max_values[-1]:
+            self._max_values.append(val)
     
-    def pop() -> int:
+    def pop(self) -> int:
         """Remove the top element from the stack"""
-        raise NotImplementedError()
+        result = self._data.pop()
+        if result >= self._max_values[-1]:
+            self._max_values.pop()
+        
+        return result
     
-    def max() -> int:
+    def max(self) -> int:
         """Return the maximal element in the stack"""
-        raise NotImplementedError()
+        if len(self._max_values) < 2:
+            raise IndexError()
+        return self._max_values[-1]
+    
+st = MyStackInt()
+st.push(1)
+st.push(2)
+print(st.max())
+st.pop()
+print(st.max())
