c1b9edc404ccd0a6790bd9590e42efb177ecb147
srgsuh <srg.suhoverhov@gmail.com>
2025-10-16 10:57:24 +0300

    Implement version with only one stack

--- main.py
+++ main.py
@@ -3,22 +3,15 @@ import math
 class MyStackInt:
     def __init__(self):
         self._data = []
-        self._max_values = []
     
     def push(self, val: int) -> None:
         """Add new element to the top of the stack"""
-        self._data.append(val)
-        if val >= (self._max_values[-1] if self.max_values else val):
-            self._max_values.append(val)
+        self._data.append((val, max(self.max() if self._data else val, val)))
     
     def pop(self) -> int:
         """Remove the top element from the stack"""
-        result = self._data.pop()
-        if result >= self._max_values[-1]:
-            self._max_values.pop()
-        
-        return result
+        return self._data.pop()[0]
     
     def max(self) -> int:
         """Return the maximal element in the stack"""
-        return self._max_values[-1]
+        return self._data[-1][1]
