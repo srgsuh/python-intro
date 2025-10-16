bf26fc66aad52ece634131b5d61198e590e41127
srgsuh <srg.suhoverhov@gmail.com>
2025-10-16 13:16:11 +0300

    Refactor MyStackInt to work with the array module

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-import math
+from array import array
 
 class MyStackInt:
     def __init__(self):
-        self._data = []
+        self._data = array('i')
+        self._max = array('i')
     
     def push(self, val: int) -> None:
-        """Add new element to the top of the stack"""
-        self._data.append((val, max(self.max() if self._data else val, val)))
+        """Add a new element to the top of the stack"""
+        self._data.append(val)
+        if not self._max or val >= self.max():
+            self._max.append(val)
     
     def pop(self) -> int:
         """Remove the top element from the stack"""
-        return self._data.pop()[0]
+        val = self._data.pop()
+        if val == self.max():
+            self._max.pop()
+        return val
     
     def max(self) -> int:
         """Return the maximal element in the stack"""
-        return self._data[-1][1]
+        return self._max[-1]
