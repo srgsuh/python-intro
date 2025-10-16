8d01012c1a5e75f0ac43cbd37b9df2b8f44e9453
srgsuh <srg.suhoverhov@gmail.com>
2025-10-16 09:51:06 +0300

    MyStackInt class definition

---
+++ main.py
@@ -0,0 +1,16 @@
+
+class MyStackInt:
+    def __init__(self):
+        pass
+    
+    def push(val: int) -> None:
+        """Add new element to the top of the stack"""
+        raise NotImplementedError()
+    
+    def pop() -> int:
+        """Remove the top element from the stack"""
+        raise NotImplementedError()
+    
+    def max() -> int:
+        """Return the maximal element in the stack"""
+        raise NotImplementedError()
