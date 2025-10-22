5af2fd44a877d151fdf5f4643d5cbc2963da14b0
srgsuh <srg.suhoverhov@gmail.com>
2025-10-22 10:53:15 +0300

    Implementation with storing index of maximum element in data array

--- main.py
+++ main.py
@@ -3,18 +3,27 @@ from array import array
 class MyStackInt:
     def __init__(self):
         self._data = array('i')
-        self._max = array('i')
+        self._max_idx = array('i')
+
+    def _index_of_max(self):
+        return self._max_idx[-1]
+
+    def __len__(self):
+        return len(self._data)
     
     def push(self, val: int) -> None:
         """Add a new element to the top of the stack"""
         self._data.append(val)
-        self._max.append(max(val, self.max() if self._max else val))
+        if (not self._max_idx) or val > self.max():
+            self._max_idx.append(len(self) - 1)
     
     def pop(self) -> int:
         """Remove the top element from the stack"""
-        self._max.pop()
-        return self._data.pop()
+        result = self._data.pop()
+        if self._index_of_max() >= len(self):
+            self._max_idx.pop()
+        return result
     
     def max(self) -> int:
         """Return the maximal element in the stack"""
-        return self._max[-1]
+        return self._data[self._index_of_max()]
