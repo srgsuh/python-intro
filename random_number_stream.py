19d86639d66501c290ff2fccf6e52bc0ba71035d
srgsuh <srg.suhoverhov@gmail.com>
2025-10-28 11:27:41 +0200

    Implement RandomNumbersStream using builder pattern

--- random_number_stream.py
+++ random_number_stream.py
@@ -1,21 +1,72 @@
 import itertools as it
-from typing import Callable, Iterator, Iterable
+from typing import Callable, Iterator
+from random import Random
+
+def unique_filter(iterator: Iterator[int]) -> Iterator[int]:
+    yielded_values: set[int] = set()
+    for x in iterator:
+        if x not in yielded_values:
+            yielded_values.add(x)
+            yield x
+
+class RandomStreamBuilder:
+    _gen: Random = Random()
+
+    def __init__(self, min: int, max: int):
+        self.stream: Iterator[int] = (self._gen.randint(l, r) for l, r in it.repeat((min, max)))
+    
+    def filter(self, callable: Callable[[int], bool] | None = None) -> "RandomStreamBuilder":
+        self.stream = self.stream if callable is None else (v for v in self.stream if callable(v))
+        return self
+
+    def limit(self, limit: int = 0) -> "RandomStreamBuilder":
+        self.stream = self.stream if not limit else it.islice(self.stream, limit)
+        return self
+    
+    def unique(self, is_unique: bool = True) -> "RandomStreamBuilder":
+        self.stream = unique_filter(self.stream) if is_unique else self.stream
+        return self
+
+    def build(self) -> Iterator[int]:
+        return self.stream
 
 class RandomNumbersStream:
-    def __init__(self,min: int = -10 ** 20, max: int = 10 ** 20):
-        raise NotImplementedError()
+    def __init__(self, min: int = -10 ** 20, max: int = 10 ** 20):
+        if max < min:
+            raise ValueError("max cannot be less then min")
+        self._max = max
+        self._min = min
+        self._limit = 0
+        self._filter: Callable[[int], bool] | None = None
+        self._is_unique = False
     
     def setFilter(self, predicate: Callable[[int], bool]):
-        raise NotImplementedError()
+        self._filter = predicate
     
     def setLimit(self, limit: int):
-        raise NotImplementedError()
+        """Limit maximum amount of values the stream might produce. The limit of 0 means unlimited"""
+        if limit < 1:
+            raise ValueError("Limit must be non-negative")
+        self._limit = limit
     
     def setDistinct(self):
-        raise NotImplementedError()
+        self._is_unique = True
     
     def resetDistinct(self):
-        raise NotImplementedError()
+        self._is_unique = False
     
     def __iter__(self) -> Iterator[int]:
-        raise NotImplementedError()
+        builder = RandomStreamBuilder(self._min, self._max)
+        return builder.filter(self._filter).unique(self._is_unique).limit(self._limit).build()
+        
+    
+if __name__ == "__main__":
+    stream = RandomNumbersStream(10, 100)
+    stream.setLimit(25)
+    it1 = iter(stream)
+    stream.setFilter(lambda x: x % 2 == 0)
+    stream.setDistinct()
+    it2 = iter(stream)
+
+    print([x for x in it1])
+    print([x for x in it2])
